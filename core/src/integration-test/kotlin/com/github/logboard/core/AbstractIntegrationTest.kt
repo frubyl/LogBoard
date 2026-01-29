e6362a2e4b8c320e85e8e4744d8a8f3d4982fb4e
Пальцева Злата Игоревна <z.paltseva@tbank.ru>
2026-01-29 17:08:14 +0300

    [feature] Добавить функционал аутентификации

--- core/src/integration-test/kotlin/com/github/logboard/core/AbstractIntegrationTest.kt
+++ core/src/integration-test/kotlin/com/github/logboard/core/AbstractIntegrationTest.kt
@@ -1,6 +1,7 @@
 package com.github.logboard.core
 
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
@@ -8,6 +9,7 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @Testcontainers
 abstract class AbstractIntegrationTest {
 
