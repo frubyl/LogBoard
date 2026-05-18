23fb83c66bd4255261754a4cffa32acc9603a349
Пальцева Злата <zlatapalceva@gmail.com>
2026-05-18 17:51:53 +0300

    [fix] PostgreSQLConfig: явные @Value вместо DataSourceProperties; починить docker-compose.yml

--- log-service/src/main/kotlin/com/github/logboard/log/config/PostgreSQLConfig.kt
+++ log-service/src/main/kotlin/com/github/logboard/log/config/PostgreSQLConfig.kt
@@ -1,7 +1,7 @@
 package com.github.logboard.log.config
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.boot.context.properties.ConfigurationProperties
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -12,13 +12,19 @@ import javax.sql.DataSource
 @Profile("!test")
 class PostgreSQLConfig {
 
-    @Bean
+    @Bean("dataSource")
     @Primary
-    @ConfigurationProperties("spring.datasource")
-    fun dataSourceProperties(): DataSourceProperties = DataSourceProperties()
-
-    @Bean
-    @Primary
-    fun dataSource(properties: DataSourceProperties): DataSource =
-        properties.initializeDataSourceBuilder().build()
+    fun dataSource(
+        @Value("\${spring.datasource.url}") url: String,
+        @Value("\${spring.datasource.username}") username: String,
+        @Value("\${spring.datasource.password}") password: String
+    ): DataSource = HikariDataSource().apply {
+        jdbcUrl = url
+        this.username = username
+        this.password = password
+        driverClassName = "org.postgresql.Driver"
+        maximumPoolSize = 10
+        minimumIdle = 2
+        poolName = "postgresql-pool"
+    }
 }
